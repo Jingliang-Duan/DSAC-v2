ca9f69e120519bf48ed0fca1fc33dc77e303a499
Liming Xiao <109412594+drsssssss@users.noreply.github.com>
2023-07-21 14:52:51 +0800

    Add files via upload

---
+++ run_figures.py
@@ -0,0 +1,25 @@
+#  Copyright (c). All Rights Reserved.
+#  General Optimal control Problem Solver (GOPS)
+#  Intelligent Driving Lab (iDLab), Tsinghua University
+#
+#  Creator: iDLab
+#  Lab Leader: Prof. Shengbo Eben Li
+#  Email: lisb04@gmail.com
+#
+#  Description: run a closed-loop system
+#  Update: 2022-12-05, Congsheng Zhang: create file
+
+
+from utils.sys_run import PolicyRunner
+
+runner = PolicyRunner(
+    log_policy_dir_list=["./results/230721-143613"],
+    trained_policy_iteration_list=["7400_opt"],
+    is_init_info=False,
+    init_info={"init_state": [-1, 0.05, 0.05, 0, 0.1, 0.1]},
+    save_render=True,
+    legend_list=["DSAC"],
+    dt=0.01, # time interval between steps
+)
+
+runner.run()
