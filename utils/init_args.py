0514aae036f95b866f622628fab1517856f4a8d0
Liming Xiao <109412594+drsssssss@users.noreply.github.com>
2023-08-31 23:16:27 +0800

    add limitation of torch/numpy CPU threads num

--- utils/init_args.py
+++ utils/init_args.py
@@ -9,7 +9,9 @@ from .common_utils import change_type, seed_everything
 
 
 def init_args(env, **args):
-    args["algorithm"] = "DSAC"
+    # set torch parallel threads nums
+    torch.set_num_threads(4)
+    print("limit torch intra-op parallel threads num to {num} for saving computing resource.".format(num = 4))
     # cuda
     if args["enable_cuda"]:
         if torch.cuda.is_available():
