820ab3475b6296a1b574646abcd5e0b01bc15389
Liming Xiao <109412594+drsssssss@users.noreply.github.com>
2024-02-23 01:00:52 +0800

    Update run_policy.py

--- run_policy.py
+++ run_policy.py
@@ -3,7 +3,7 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 runner = PolicyRunner(
     log_policy_dir_list=["./results/DSAC_V2_gym_pendulum/240223-003213"],
-    trained_policy_iteration_list=["5000"],
+    trained_policy_iteration_list=["8900_opt"],
     is_init_info=False,
     init_info={"init_state": [-1, 0.05, 0.05, 0, 0.1, 0.1]},
     save_render=True,
